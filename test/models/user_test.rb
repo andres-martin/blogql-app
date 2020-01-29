9b6e36d5b5b66aad085ef12a3a4d47abdaf11347
andres-martin <andresmartin266@gmail.com>
2020-01-28 20:57:03 -0500

    [add] annotate all models

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  first_name :string
+#  last_name  :string
+#  street     :string
+#  number     :integer
+#  city       :string
+#  postcode   :string
+#  country    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
